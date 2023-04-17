abe16454f16d143674a594dce8bff2b69c9c7102
Arxbird <130928874+Arxbird@users.noreply.github.com>
2023-04-17 15:47:57 +0200

    Refactured Code

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-window.onload = function() {
-    var daysLeft = document.getElementById("daysLeft");
+function daysLeftToString(daysDiff) {
+    return Math.floor(daysDiff) + " Days Left"
+}
 
+function getDaysLeft() {
     var currentDate = new Date().getTime();
     var summerBreakDate = new Date("06/09/2023");
 
     var timeDiff = summerBreakDate - currentDate;
     var daysDiff = timeDiff / (1000 * 60 * 60 * 24);
+    return daysLeftToString(daysDiff)
+}
 
-    var text = Math.floor(daysDiff) + " Days Left"
-    
-    daysLeft.innerHTML = text;
+window.onload = function() {
+    var daysLeft = document.getElementById("daysLeft");
+    var text = getDaysLeft()
+    daysLeft.innerHTML = text
 }
