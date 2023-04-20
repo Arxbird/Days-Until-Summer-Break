9022a50c660ed84bd1d62c576bba495066e5a1f2
Arxbird <130928874+Arxbird@users.noreply.github.com>
2023-04-20 23:02:30 +0200

    Final Release
    - Changed Meta Tags
    - Added Tab Icon
    - Added Spinning Icon on Website

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
-function daysLeftToString(daysDiff) {
-    return Math.floor(daysDiff) + " Days Left"
-}
-
-function getDaysLeft() {
-    var currentDate = new Date().getTime();
-    var summerBreakDate = new Date("06/09/2023");
-
-    var timeDiff = summerBreakDate - currentDate;
-    var daysDiff = timeDiff / (1000 * 60 * 60 * 24);
-    return daysLeftToString(daysDiff)
-}
-
-window.onload = function() {
-    var daysLeft = document.getElementById("daysLeft");
-    var text = getDaysLeft()
-    daysLeft.innerHTML = text
-}
+function getTimeDifference(date) {
+    const futureDate = new Date(date);
+    const currentDate = new Date();
+    const timeDifference = futureDate - currentDate;
+  
+    if (timeDifference <= 0) {
+      return "Summer break!";
+    }
+  
+    const daysLeft = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hoursLeft = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutesLeft = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const secondsLeft = Math.floor((timeDifference % (1000 * 60)) / 1000);
+  
+    if (daysLeft > 1) {
+      return `${daysLeft} days left`;
+    } else {
+      return `${hoursLeft} hours, ${minutesLeft} minutes, ${secondsLeft} seconds left`;
+    }
+  }
+  
+  
+  function updateDaysLeft() {
+    const summerBreakDate = new Date(1686315600 * 1000);
+    const timezoneOffset = summerBreakDate.getTimezoneOffset() * 60 * 1000;
+    const localSummerBreakDate = new Date(summerBreakDate.getTime() + timezoneOffset);
+    const daysLeftElement = document.getElementById("daysLeft");
+    const text = getTimeDifference(localSummerBreakDate);
+    daysLeftElement.innerHTML = text;
+  
+    window.requestAnimationFrame(updateDaysLeft);
+  }
+  
+  window.onload = function() {
+    updateDaysLeft();
+  };
+  
