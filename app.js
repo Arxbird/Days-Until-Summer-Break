4d79759f0e961501cc4ab81205c80250c547bd1f
Arxbird <130928874+Arxbird@users.noreply.github.com>
2023-05-17 13:57:56 +0200

    Changed Timestamp

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function getTimeDifference(date) {
   
   
   function updateDaysLeft() {
-    const summerBreakDate = new Date(1686315600 * 1000);
+    const summerBreakDate = new Date(1686175200 * 1000);
     const timezoneOffset = summerBreakDate.getTimezoneOffset() * 60 * 1000;
     const localSummerBreakDate = new Date(summerBreakDate.getTime() + timezoneOffset);
     const daysLeftElement = document.getElementById("daysLeft");
