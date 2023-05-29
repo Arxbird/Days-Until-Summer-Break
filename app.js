18db10313e6c592feb7553d178eabba703a75d2b
Arxbird <130928874+Arxbird@users.noreply.github.com>
2023-05-29 22:55:21 +0200

    Fixed Timestamp Issue (FINALLY)

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 function getTimeDifference(date) {
-    const futureDate = new Date(date);
-    const currentDate = new Date();
-    const timeDifference = futureDate - currentDate;
-  
-    if (timeDifference <= 0) {
-      return "Summer break!";
-    }
-  
-    const daysLeft = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  const futureDate = new Date(date);
+  const currentDate = new Date();
+  const timeDifference = futureDate - currentDate;
+
+  if (timeDifference <= 0) {
+    return "Summer break!";
+  }
+
+  const daysLeft = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+
+  if (daysLeft > 1) {
+    return `${daysLeft} days left`;
+  } else {
     const hoursLeft = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutesLeft = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
     const secondsLeft = Math.floor((timeDifference % (1000 * 60)) / 1000);
-  
-    if (daysLeft > 1) {
-      return `${daysLeft} days left`;
-    } else {
-      return `${hoursLeft} hours, ${minutesLeft} minutes, ${secondsLeft} seconds left`;
-    }
+    return `${hoursLeft} hours, ${minutesLeft} minutes, ${secondsLeft} seconds left`;
   }
+}
   
   
   function updateDaysLeft() {
